4ba78d6fb831740b05fc8eac9afce82e6335c661
Snova1054 <istar.dark@gmail.com>
2021-12-10 11:22:52 -0500

    trying the {config.force_ssl = true} option

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   mount Blazer::Engine, at: "blazer"
   devise_for :users
+  # scope defaults: (Rails.env.production? ? { protocol: 'https' } : {}) do
+  #   devise_for :users
+  # end
+  # scope defaults: (Rails.env.development? ? { protocol: 'https' } : {}) do
+  #   devise_for :users
+  # end
   root 'home#index'
   resources :home
   resources :quotes
