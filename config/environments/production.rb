afca3c93dfd6c096189d9fe697b2e0ccf217607f
Daniel Gallardo <istar.dark@gmail.com>
2021-12-10 18:58:25 -0500

    disabled force_ssl

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
-  # config.to_prepare { Devise::SessionsController.force_ssl }
-  # config.to_prepare { Devise::RegistrationsController.force_ssl }
-  # config.to_prepare { Devise::PasswordsController.force_ssl }
+  config.to_prepare { Devise::SessionsController.force_ssl }
+  config.to_prepare { Devise::RegistrationsController.force_ssl }
+  config.to_prepare { Devise::PasswordsController.force_ssl }
   # Settings specified here will take precedence over those in config/application.rb.
   config.action_mailer.default_url_options = { protocol: 'https', host: "blazer.dokkuapp.com"}
   # Code is not reloaded between requests.
@@ -50,7 +50,7 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = true
+  # config.force_ssl = true
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
