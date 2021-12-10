c7bb31d81cdf1e99205748c6a15336ab5dc3e29f
Daniel Gallardo Ishii <dgallardo@edu.cegepgarneau.ca>
2021-12-10 11:40:28 -0500

    trying {config.to_prepare} again but in dev.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   config.to_prepare { Devise::SessionsController.force_ssl }
   config.to_prepare { Devise::RegistrationsController.force_ssl }
-  
+  # Settings specified here will take precedence over those in config/application.rb.
   config.action_mailer.default_url_options = {host: "blazer.dokkuapp.com"}
   # Settings specified here will take precedence over those in config/application.rb.
 
