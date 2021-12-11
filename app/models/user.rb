918d142a00a456dfc6899ec6dc3cb1b882ff5dbf
Daniel Gallardo <istar.dark@gmail.com>
2021-12-10 19:40:34 -0500

    couldnt add authenticity token for the deploy, removed it

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :authenticity_token
+         :recoverable, :rememberable, :validatable
   validate :password_special
 
   def password_special
