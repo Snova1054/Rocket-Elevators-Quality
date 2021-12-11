ba401c686f00729f47be27f47febaf2484f44323
Daniel Gallardo <istar.dark@gmail.com>
2021-12-10 20:49:07 -0500

    commenting the password verification

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validate :password_special
+  # validate :password_special
 
-  def password_special
-    return if password.blank? || password =~ /\A(?=.*\d)(?=.*[A-Z])(?=.*\W)[^ ]{8,}\z/
-    errors.add :password, 'Password should contain a minimum of 8 characters including 1 uppercase letter, 1 number, 1 special character'
-  end
+  # def password_special
+  #   return if password.blank? || password =~ /\A(?=.*\d)(?=.*[A-Z])(?=.*\W)[^ ]{8,}\z/
+  #   errors.add :password, 'Password should contain a minimum of 8 characters including 1 uppercase letter, 1 number, 1 special character'
+  # end
 end
