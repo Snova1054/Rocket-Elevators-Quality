f374c99f15048bdb88718539590dfc081c2e303a
Daniel Gallardo <istar.dark@gmail.com>
2021-12-10 20:57:14 -0500

    added back devise_for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   mount Blazer::Engine, at: "blazer"
-  # devise_for :users
   root 'home#index'
   resources :home
+  devise_for :users
+  # devise_for :users, path: '', protocol: "https", path_names: { sign_in: 'users/sign_in', sign_out: 'users/sign_out' }
   resources :quotes
   resources :interventions
-  get 'quote', to: 'quotes#new'
   get 'intervention', to: 'interventions#index'
+  get 'quote', to: 'quotes#new'
   get 'commercial', to: 'home#commercial'
   get 'residential', to: 'home#residential'
   get 'map', to: 'map#index'
