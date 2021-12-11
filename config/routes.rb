7efbb58eb2bfd9973eb0c037a40323ad54d24db0
Daniel Gallardo <istar.dark@gmail.com>
2021-12-10 19:01:48 -0500

    moved routes devise_for

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users, path: '', protocol: "https", path_names: { sign_in: 'login', sign_out: 'logout' }
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   mount Blazer::Engine, at: "blazer"
   root 'home#index'
-  devise_for :users, path: '', protocol: "https", path_names: { sign_in: 'login', sign_out: 'logout' }
   resources :home
   resources :quotes
   # get 'interventions/index'
