189b79ce813af5e3efb4b2985e3a60351f0843e6
Daniel Gallardo <istar.dark@gmail.com>
2021-12-10 20:30:32 -0500

    removed everything about ssl and redirection for login and signup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,8 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.action_mailer.default_url_options = { protocol: 'https', host: "rocket-elevators.xyz"}
-  config.to_prepare { Devise::SessionsController.force_ssl }
-  config.to_prepare { Devise::RegistrationsController.force_ssl }
+  # config.to_prepare { Devise::SessionsController.force_ssl }
+  # config.to_prepare { Devise::RegistrationsController.force_ssl }
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -49,7 +49,7 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = true
+  # config.force_ssl = true
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
