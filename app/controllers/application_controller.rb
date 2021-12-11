ce62662c5c9c7976d52110238ae5bcd8611663c6
Daniel Gallardo <istar.dark@gmail.com>
2021-12-10 20:14:13 -0500

    commented require_admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,15 +2,15 @@ class ApplicationController < ActionController::Base
     protect_from_forgery prepend: true, with: :exception  
     skip_before_action :verify_authenticity_token
     
-    def require_admin
-        redirect_to main_app.root_path unless current_user && current_user.admin? || current_user.employee?
-    end
+    # def require_admin
+    #     redirect_to main_app.root_path unless current_user && current_user.admin? || current_user.employee?
+    # end
 
     def after_sign_in_path_for(resource_or_scope)
-        puts("hello")
+        "https://www.rocket-elevators.xyz/"
     end
 
     def after_sign_out_path_for(resource_or_scope)
-        puts("hello")
+        "https://www.rocket-elevators.xyz/"
     end
 end
