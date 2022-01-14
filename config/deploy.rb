5e94f85808bbdcd615683545f062231f67bce724
Snova1054 <63308626+Snova1054@users.noreply.github.com>
2022-01-14 10:35:20 -0500

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ lock "~> 3.16.0"
 before 'deploy', 'rvm1:install:ruby'
 set :rvm_map_bins, [ 'rake', 'gem', 'bundle', 'ruby', 'puma', 'pumactl' ]
 set :application, "DANIEL_GALLARDO"
-set :repo_url, "git://github.com:Snova1054/Rocket-Elevators-Quality.git"
+set :repo_url, "git://github.com/Snova1054/Rocket-Elevators-Quality.git"
 set :branch, 'main'
 
 # Default branch is :main
